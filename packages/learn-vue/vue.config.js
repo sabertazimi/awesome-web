d8fc66c7a41304b48275bd4ede8e4e5bad33ced0
sabertazimi <sabertazimi@gmail.com>
2021-09-26 00:15:39 +0800

    fix(Vue-CLI): disable production source maps

--- packages/learn-vue/vue.config.js
+++ packages/learn-vue/vue.config.js
@@ -2,6 +2,7 @@
 module.exports = {
   transpileDependencies: true,
   lintOnSave: false,
+  productionSourceMap: false,
   publicPath:
     process.env.NODE_ENV === 'production' ? '/awesome-web/learn-vue/' : '/',
 };
