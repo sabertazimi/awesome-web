b7b0b296d63734716d5948a82c905b40ab3fb4f0
sabertazimi <sabertazimi@gmail.com>
2021-09-26 04:57:55 +0800

    test(Vue-learn): rectify `EventCard` testing

--- packages/learn-vue/tests/unit/example.spec.ts
+++ packages/learn-vue/tests/unit/example.spec.ts
@@ -1,12 +1,30 @@
 import { shallowMount } from '@vue/test-utils';
-import HelloWorld from '@/components/HelloWorld.vue';
+import EventCard from '@/components/EventCard.vue';
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message';
-    const wrapper = shallowMount(HelloWorld, {
-      props: { msg },
+describe('EventCard.vue', () => {
+  it('renders event information when passed', () => {
+    const event = {
+      id: 5928101,
+      title: 'Cat Adoption Day',
+      date: 'January 28, 2022',
+      time: '12:00',
+      location: 'Meow Town',
+      category: 'animal welfare',
+      description: 'Find your new feline friend at this event.',
+      organizer: 'Kat Laydee',
+      petsAllowed: true,
+    };
+    const wrapper = shallowMount(EventCard, {
+      props: { event },
     });
-    expect(wrapper.text()).toMatch(msg);
+    expect(wrapper.text()).toMatch(event.title);
+    expect(wrapper.text()).toMatch(event.date);
+    expect(wrapper.text()).toMatch(event.time);
+    expect(wrapper.text()).not.toMatch(event.id.toString());
+    expect(wrapper.text()).not.toMatch(event.location);
+    expect(wrapper.text()).not.toMatch(event.category);
+    expect(wrapper.text()).not.toMatch(event.description);
+    expect(wrapper.text()).not.toMatch(event.organizer);
+    expect(wrapper.text()).not.toMatch(event.petsAllowed.toString());
   });
 });
