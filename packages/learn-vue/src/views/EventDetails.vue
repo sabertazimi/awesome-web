fd750d580de7d943274f8c6aca80deffabc574f9
sabertazimi <sabertazimi@gmail.com>
2021-09-26 20:13:02 +0800

    fix(Vue-learn): remove redundant `v-if` directive
    With top level `suspense` component,
    no need for `v-if` directive in `EventDetails` component.

--- packages/learn-vue/src/views/EventDetails.vue
+++ packages/learn-vue/src/views/EventDetails.vue
@@ -1,9 +1,7 @@
 <template>
-  <div v-if="event">
-    <h1>{{ event.title }}</h1>
-    <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
-    <p>{{ event.description }}</p>
-  </div>
+  <h1>{{ event.title }}</h1>
+  <p>{{ event.time }} on {{ event.date }} @ {{ event.location }}</p>
+  <p>{{ event.description }}</p>
 </template>
 
 <script setup lang="ts">
