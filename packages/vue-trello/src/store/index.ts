4d2cd90f68956101024aad1c40910b24af5665d7
sabertazimi <sabertazimi@gmail.com>
2021-09-28 01:32:26 +0800

    fix(Vue-vuex): add TypeScript types for vuex

--- packages/vue-trello/src/store/index.ts
+++ packages/vue-trello/src/store/index.ts
@@ -1,13 +1,24 @@
-import { createStore } from 'vuex';
+import { InjectionKey } from 'vue';
+import { createStore, Store } from 'vuex';
 import { getDefaultBoard } from 'src/services';
 import type { BoardType } from 'src/services';
+
+interface State {
+  board: BoardType;
+}
 
 const board: BoardType =
   JSON.parse(localStorage.getItem('board') as string) || getDefaultBoard();
 
-export default createStore({
+const key: InjectionKey<Store<State>> = Symbol('state');
+
+const store = createStore<State>({
   state: { board },
   mutations: {},
   actions: {},
   modules: {},
 });
+
+export { key };
+export type { State };
+export default store;
