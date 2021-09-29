981563273f588a4b41abc94f701b2802bea709af
sabertazimi <sabertazimi@gmail.com>
2021-09-30 04:01:51 +0800

    fix(Vue-trello): refactor to `ColumnTask` component

--- packages/vue-trello/src/components/ColumnTask.vue
+++ packages/vue-trello/src/components/ColumnTask.vue
@@ -1,31 +1,31 @@
 <script setup lang="ts">
+import { useAppRouter } from 'src/router';
 import { useAppStore } from 'src/store';
-import type { BoardColumnType } from 'src/services';
-import ColumnTask from 'src/components/ColumnTask.vue';
+import type { TaskType } from 'src/services';
 
-defineProps<{ columnIndex: number; column: BoardColumnType }>();
+defineProps<{ columnIndex: number; taskIndex: number; task: TaskType }>();
 
 const store = useAppStore();
+const router = useAppRouter();
 
-const createTask = (event: Event, columnIndex: number) => {
-  const inputElement = event.target as HTMLInputElement;
-  store.commit('createTask', {
-    columnIndex,
-    name: inputElement.value,
-  });
-  inputElement.value = '';
-};
+const goToTask = (task: TaskType) =>
+  router.push({ name: 'task', params: { id: task.id } });
 
-const deleteColumn = (id: string) => store.commit('deleteColumn', { id });
+const deleteTask = (columnIndex: number, taskId: string) =>
+  store.commit('deleteTask', { columnIndex, taskId });
 
-const pickupColumn = (event: DragEvent, fromColumnIndex: number) => {
+const pickupTask = (
+  event: DragEvent,
+  fromColumnIndex: number,
+  fromTaskIndex: number
+) => {
   const dataTransfer = event.dataTransfer;
 
   if (dataTransfer) {
     dataTransfer.dropEffect = 'move';
     dataTransfer.effectAllowed = 'move';
     dataTransfer.setData('fromColumnIndex', fromColumnIndex.toString());
-    dataTransfer.setData('type', 'column');
+    dataTransfer.setData('fromTaskIndex', fromTaskIndex.toString());
   }
 };
 
@@ -75,45 +75,36 @@ const moveTaskOrColumn = (
 
 <template>
   <div
+    class="task"
     draggable="true"
     @dragenter.prevent
     @dragover.prevent
-    @dragstart.stop="pickupColumn($event, columnIndex)"
-    @drop.stop="moveTaskOrColumn($event, columnIndex)"
+    @dragstart.stop="pickupTask($event, columnIndex, taskIndex)"
+    @drop.stop="moveTaskOrColumn($event, columnIndex, taskIndex)"
+    @click.stop="goToTask(task)"
   >
-    <div class="flex items-center mb-2 font-bold">
-      <span>{{ column.name }}</span>
+    <div class="task-name">
+      <span class="px-4 py-2">{{ task.name }}</span>
       <button
-        class="btn-block danger ml-auto"
-        @click.stop="deleteColumn(column.id)"
+        class="btn-block ml-auto"
+        @click.stop="deleteTask(columnIndex, task.id)"
       >
         X
       </button>
     </div>
-    <div v-if="column.tasks">
-      <transition-group>
-        <ColumnTask
-          v-for="(task, taskIndex) in column.tasks"
-          :key="task.id"
-          :column-index="columnIndex"
-          :task-index="taskIndex"
-          :task="task"
-        />
-      </transition-group>
-      <input
-        type="text"
-        class="task-input"
-        placeholder="+ Enter new task ..."
-        @keyup.enter="createTask($event, columnIndex)"
-      />
-    </div>
+    <p v-if="task.description" class="flex-shrink-0 w-full mt-1 px-4 py-2">
+      {{ task.description }}
+    </p>
   </div>
 </template>
 
 <style scoped>
-.task-input {
-  @apply block w-full p-2 bg-transparent border border-transparent outline-none;
-  @apply focus:border-green-500;
-  @apply transition duration-500;
+.task {
+  @apply flex items-center flex-wrap mb-2;
+  @apply bg-white text-gray-900 shadow no-underline cursor-pointer;
+}
+
+.task-name {
+  @apply flex flex-row items-center flex-shrink-0 w-full font-bold;
 }
 </style>
