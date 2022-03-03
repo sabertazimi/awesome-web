3bd5480639b055e43f43bb75077c595d1c456f6f
sabertazimi <sabertazimi@gmail.com>
2022-03-03 08:02:16 +0800

    fix(vue-design): update vitrual DOM and apply to DOM

--- packages/vue-design/src/main.ts
+++ packages/vue-design/src/main.ts
@@ -4,6 +4,14 @@ import './style.css';
 const patch = init([]);
 
 const Component = (props: { title: string }) => h('h1', props.title);
-const prevVNode = Component({ title: 'prev' });
+const prevVNode = Component({ title: 'Prev State' });
+const nextVNode = Component({ title: 'Next State' });
+let state = true;
 
 patch(document.querySelector<HTMLDivElement>('#app')!, prevVNode);
+
+const button = document.querySelector('.button')!;
+button.addEventListener('click', () => {
+  state ? patch(prevVNode, nextVNode) : patch(nextVNode, prevVNode);
+  state = !state;
+});
