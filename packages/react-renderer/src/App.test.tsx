9de9d3ec73c066ff405e26a92cabca41d7640406
sabertazimi <sabertazimi@gmail.com>
2022-03-24 20:39:46 +0800

    test(react-renderer): add tests for counter buttons

--- packages/react-renderer/src/App.test.tsx
+++ packages/react-renderer/src/App.test.tsx
@@ -1,13 +1,26 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
   const incElement = screen.getByText(/\+/i);
   const decElement = screen.getByText(/-/i);
-  const countElement = screen.getByText(/0/i);
 
   expect(incElement).toBeInTheDocument();
   expect(decElement).toBeInTheDocument();
-  expect(countElement).toBeInTheDocument();
+  expect(screen.getByText(0)).toBeInTheDocument();
+
+  fireEvent.click(incElement);
+  expect(screen.getByText(1)).toBeInTheDocument();
+  fireEvent.click(incElement);
+  expect(screen.getByText(2)).toBeInTheDocument();
+  fireEvent.click(incElement);
+  expect(screen.getByText(3)).toBeInTheDocument();
+
+  fireEvent.click(decElement);
+  expect(screen.getByText(2)).toBeInTheDocument();
+  fireEvent.click(decElement);
+  expect(screen.getByText(1)).toBeInTheDocument();
+  fireEvent.click(decElement);
+  expect(screen.getByText(0)).toBeInTheDocument();
 });
