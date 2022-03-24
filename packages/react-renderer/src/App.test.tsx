9aa6ab6bf9ab7ba79b2aac381194bb57afc3d52e
sabertazimi <sabertazimi@gmail.com>
2022-03-24 20:14:27 +0800

    test(react-renderer): rectify broken tests

--- packages/react-renderer/src/App.test.tsx
+++ packages/react-renderer/src/App.test.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const incElement = screen.getByText(/\+/i);
+  const decElement = screen.getByText(/-/i);
+  const countElement = screen.getByText(/0/i);
+
+  expect(incElement).toBeInTheDocument();
+  expect(decElement).toBeInTheDocument();
+  expect(countElement).toBeInTheDocument();
 });
