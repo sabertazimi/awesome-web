64f9d62617bcd3c834c7b2fd143336ba10821148
Sabertaz <sabertazimi@gmail.com>
2022-03-24 20:58:16 +0800

    test(react-renderer): rectify broken tests (#296)

--- packages/react-renderer/src/App.test.tsx
+++ packages/react-renderer/src/App.test.tsx
@@ -1,9 +1,32 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const incElement = screen.getByText(/\+/i);
+  const decElement = screen.getByText(/-/i);
+  const countElement = screen.getByText(0);
+
+  expect(incElement).toBeInTheDocument();
+  expect(decElement).toBeInTheDocument();
+  expect(countElement).toBeInTheDocument();
+  expect(countElement.textContent).toBe('0');
+
+  fireEvent.click(incElement);
+  expect(countElement.textContent).toBe('1');
+  fireEvent.click(incElement);
+  expect(countElement.textContent).toBe('2');
+  fireEvent.click(incElement);
+  expect(countElement.textContent).toBe('3');
+
+  fireEvent.click(decElement);
+  expect(countElement.textContent).toBe('2');
+  fireEvent.click(decElement);
+  expect(countElement.textContent).toBe('1');
+  fireEvent.click(decElement);
+  expect(countElement.textContent).toBe('0');
+  fireEvent.click(decElement);
+  expect(countElement.textContent).toBe('-1');
+  fireEvent.click(decElement);
+  expect(countElement.textContent).toBe('-2');
 });
