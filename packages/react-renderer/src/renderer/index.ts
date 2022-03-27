4b3f8a6af95096df782b08569c898a741cf3da98
Sabertaz <sabertazimi@gmail.com>
2022-03-27 19:58:10 +0800

    fix(react-renderer): implement React 18 style renderer (#302)

--- packages/react-renderer/src/renderer/index.ts
+++ packages/react-renderer/src/renderer/index.ts
@@ -1,16 +1,20 @@
 import type { ReactElement } from 'react';
 import Reconciler from './reconciler';
-import type { Container } from './types';
+import type { Container, OpaqueRoot } from './types';
 
 const Renderer = {
-  render: (
-    element: ReactElement,
+  createRoot: (
     container: Container | null,
     callback?: Function
-  ) => {
+  ): OpaqueRoot => {
     if (container) {
       const root = Reconciler.createContainer(container, 0, false, null);
-      Reconciler.updateContainer(element, root, null);
+
+      root.render = function (element: ReactElement) {
+        Reconciler.updateContainer(element, this, null);
+      };
+
+      return root;
     }
   },
 };
