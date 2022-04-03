575e8c8ea75c4c8c57a95ba7d24ab66a9938fa1d
Sabertaz <sabertazimi@gmail.com>
2022-04-03 19:12:13 +0800

    chore(deps): update bod monorepo to ^5.8.1 (#308)

--- packages/vue-basis/src/views/EventList.vue
+++ packages/vue-basis/src/views/EventList.vue
@@ -1,10 +1,3 @@
-<template>
-  <div class="events">
-    <h1>Events List</h1>
-    <EventCard v-for="event in events" :key="event.id" :event="event" />
-  </div>
-</template>
-
 <script setup lang="ts">
 import EventCard from '@/components/EventCard.vue'; // @ is an alias to /src
 import { getEvents } from '@/services';
@@ -12,6 +5,13 @@ import type { Event } from '@/services';
 
 const events: Event[] = await getEvents();
 </script>
+
+<template>
+  <div class="events">
+    <h1>Events List</h1>
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+  </div>
+</template>
 
 <style scoped>
 .events {
