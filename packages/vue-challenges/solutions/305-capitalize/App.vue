dca4e73d3641dd03071ee869d0eba24087180f47
Sabertaz <sabertazimi@gmail.com>
2022-08-25 22:55:55 +0800

    fix(vue-challenges): complete custom directive modifier challenge (#374)

---
+++ packages/vue-challenges/solutions/305-capitalize/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { ref, vModelText } from 'vue';
+
+vModelText.updated = (el, { value, modifiers: { capitalize } }) => {
+  if (capitalize && Object.hasOwn(value, 0)) {
+    el.value = value[0].toUpperCase() + value.slice(1);
+  }
+};
+
+const value = ref('');
+</script>
+
+<template>
+  <!-- eslint-disable-next-line vue/valid-v-model -->
+  <input v-model.capitalize="value" type="text" />
+</template>
