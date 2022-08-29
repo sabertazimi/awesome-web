972cc9f18be6587f1de946acf053b0ccc72837b7
sabertazimi <sabertazimi@gmail.com>
2022-08-30 00:27:34 +0800

    fix(vue-challenges): custom watch effect directive

--- packages/vue-challenges/solutions/24-v-active-style/App.vue
+++ packages/vue-challenges/solutions/24-v-active-style/App.vue
@@ -1,12 +1,32 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
+import type { CSSProperties } from 'vue';
+
+interface Binding {
+  value: [CSSProperties, () => boolean];
+}
 
 /**
  * Implement the custom directive
  * Make sure the list item text color changes to red when the `toggleTab` is toggled
  *
  */
-const VActiveStyle = {};
+const vActiveStyle = {
+  mounted(el: HTMLLIElement, { value }: Binding) {
+    const [styles, fn] = value;
+    watch(
+      fn,
+      newValue => {
+        for (const [name, style] of Object.entries(styles)) {
+          el.style[name] = newValue ? style : '';
+        }
+      },
+      {
+        immediate: true,
+      }
+    );
+  },
+};
 
 const list = [1, 2, 3, 4, 5, 6, 7, 8];
 const activeTab = ref(0);
