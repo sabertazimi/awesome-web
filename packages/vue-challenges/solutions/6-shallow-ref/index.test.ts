f430596ee85687889cf4a785f661473bdfbaacc9
sabertazimi <sabertazimi@gmail.com>
2022-08-29 21:08:16 +0800

    test(vue-challenges): remove console output

--- packages/vue-challenges/solutions/6-shallow-ref/index.test.ts
+++ packages/vue-challenges/solutions/6-shallow-ref/index.test.ts
@@ -5,7 +5,7 @@ import App from './App.vue';
 
 describe('shallowRef', () => {
   it('should work', () => {
-    const spy = vi.spyOn(console, 'log');
+    const spy = vi.spyOn(console, 'log').mockImplementation(vi.fn(() => {}));
     mount(App);
     expect(spy).toHaveBeenCalled();
   });
