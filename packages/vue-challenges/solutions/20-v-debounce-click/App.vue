70d9188c50b9563ddf28843ac814b883a69158cf
sabertazimi <sabertazimi@gmail.com>
2022-08-29 22:57:45 +0800

    fix(vue-challenges): debounce click custom directive

--- packages/vue-challenges/solutions/20-v-debounce-click/App.vue
+++ packages/vue-challenges/solutions/20-v-debounce-click/App.vue
@@ -6,7 +6,21 @@
  *
  */
 
-const VDebounceClick = {};
+const debounce = (fn: Function, delay: number) => {
+  let timer = null;
+
+  return function () {
+    if (timer) return;
+    fn();
+    timer = setTimeout(() => (timer = clearTimeout(timer)), delay);
+  };
+};
+
+const vDebounceClick = {
+  mounted(el: HTMLElement, { value, arg }: { value: Function; arg: number }) {
+    el.addEventListener('click', debounce(value, arg));
+  },
+};
 
 function onClick() {
   console.log('Only triggered once when clicked many times quickly');
