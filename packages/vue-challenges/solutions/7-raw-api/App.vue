94973991a7216a0656f8dbb296835d7f56352350
sabertazimi <sabertazimi@gmail.com>
2022-08-29 20:57:32 +0800

    fix(vue-challenges): `markRaw` and `toRaw` API

--- packages/vue-challenges/solutions/7-raw-api/App.vue
+++ packages/vue-challenges/solutions/7-raw-api/App.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { isReactive, reactive } from 'vue';
+import { isReactive, markRaw, reactive, toRaw } from 'vue';
 
 const state = { count: 1 };
 const reactiveState = reactive(state);
@@ -7,12 +7,12 @@ const reactiveState = reactive(state);
 /**
  * Modify the code so that we can make the output be true.
  */
-console.log(reactiveState === state);
+console.log(toRaw(reactiveState) === state);
 
 /**
  * Modify the code so that we can make the output be false.
  */
-const info = { count: 1 };
+const info = markRaw({ count: 1 });
 const reactiveInfo = reactive(info);
 
 console.log(isReactive(reactiveInfo));
