20c12c44711a467f94708b096c1e14518b03105e
sabertazimi <sabertazimi@gmail.com>
2022-08-30 01:27:52 +0800

    fix(vue-challenges): `h()` render function

--- packages/vue-challenges/questions/218-h-render-function/MyButton.ts
+++
@@ -1,8 +0,0 @@
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'MyButton',
-  render() {
-    return h(/** do someting */);
-  },
-});
---
+++ packages/vue-challenges/solutions/218-h-render-function/MyButton.ts
@@ -0,0 +1,21 @@
+import { defineComponent, h } from 'vue';
+
+export default defineComponent({
+  name: 'MyButton',
+  props: {
+    disabled: Boolean,
+  },
+  emits: ['custom-click'],
+  render() {
+    return h(
+      'button',
+      {
+        disabled: this.disabled,
+        onClick: () => {
+          if (!this.disabled) this.$emit('custom-click');
+        },
+      },
+      this.$slots.default?.()
+    );
+  },
+});
