feadcf37a956ca0f5ffc1533e17f5e8191e71bc7
sabertazimi <sabertazimi@gmail.com>
2022-08-30 01:20:18 +0800

    fix(vue-challenges): props validation

--- packages/vue-challenges/questions/323-prop-validation/Button.vue
+++
@@ -1,9 +0,0 @@
-<script setup>
-defineProps({
-  type: {},
-});
-</script>
-
-<template>
-  <button>Button</button>
-</template>
---
+++ packages/vue-challenges/solutions/323-prop-validation/Button.vue
@@ -0,0 +1,17 @@
+<script setup>
+defineProps({
+  type: {
+    type: String,
+    default: 'default',
+    validator(value) {
+      return ['primary', 'ghost', 'dashed', 'link', 'text', 'default'].includes(
+        value
+      );
+    },
+  },
+});
+</script>
+
+<template>
+  <button>Button</button>
+</template>
